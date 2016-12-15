96e73d782a9115a1991471b2df4d124e1eb4683a
Blake Guilloud <fbguillo@gmail.com>
2016-12-15 08:31:00 -0500

    Included key as well as code

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ console.log('hello');
 
 function renderCode(event) {
   const code = event.keyCode;
+  const key = event.code;
+  console.log('event', event);
 
-  document.getElementById('root').innerHTML = code;
+  document.getElementById('code').innerHTML = code;
+  document.getElementById('key').innerHTML = key;
 }
 
 window.addEventListener('keydown', renderCode);
